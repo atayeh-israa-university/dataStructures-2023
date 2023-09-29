875cda2126210be95c2889d8d27db3893e5d4200
dr-ahmedtayeh <ahmed.ao.tayeh@gmail.com>
2023-09-29 21:08:34 +0200

    arrays

---
+++ Theory - EITM2311/Arrays/SingularArrays.java
@@ -0,0 +1,29 @@
+	
+	
+	import java.util.*;
+
+	public class SingularArrays {
+		public static void main(String args[]) {
+			
+	   String [] students = {"Hassan", "Anas", "Khadija"};
+	   String [] courses = new String [5]; //All initialized with NULL 
+	   
+	   for(int i =0; i< students.length; i++){
+		   System.out.print(students[i] + "\t"); //Output Hassan	Anas	Khadija	
+	   }
+	   
+	   System.out.println("\n"+ students.length); //Output 3
+	   System.out.println(courses.length); //Output 5
+	   
+	   System.out.println(Arrays.asList(courses).size()); //Output 5
+	   
+	   for (String s: courses){
+			System.out.print(s + "\t"); //Output NULL NULL NULL NULL NULL
+	   }
+	   
+	   
+		}
+	}
+	
+	
+	
