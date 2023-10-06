4558bfe086801d4bd5dacbffe02a31e35a2260b9
dr-ahmedtayeh <ahmed.ao.tayeh@gmail.com>
2023-10-06 13:24:39 +0200

    2D array

---
+++ Theory - EITM2311/Multi-Dimensional-Arrays/TwoDimensionalArray.java
@@ -0,0 +1,26 @@
+	
+	public class TwoDimensionalArray {
+		public static void main(String[] args) {
+			int rows = 5;
+			int columns = 5;
+	 		int[][] array = new int[rows][columns];
+	 		int value = 20;
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					array[i][j] = value;
+					value++;
+				}
+			}	 
+			System.out.println("The 2D array is: ");
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					System.out.print(array[i][j] + " ");
+				}
+				System.out.println();
+			}		   
+		}		
+	}
+	
+	
+	
+	
